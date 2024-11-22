9cdeb6bd4ef548c656002ac6a6e63fe52e94647c
Shubham Mishra <shubhammishra10101998@gmail.com>
2024-11-22 19:57:57 +0530

    UP TO DATE

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -3,20 +3,11 @@ import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
 import { role, teachersData } from '@/lib/data';
+import { Class, Subject, Teacher } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link';
 
-type Teacher = {
-  id: number;
-  teacherId: number;
-  name: string;
-  email?: string;
-  photo: string;
-  phone: string;
-  subjects: string[];
-  classes: string[];
-  address: string;
-};
+type TeacherList = Teacher & { subjects: Subject[] } & { classes: Class[] };
 
 const columns = [
   {
@@ -54,41 +45,43 @@ const columns = [
   },
 ];
 
-export default function TeachersList() {
-  const renderRow = (items: Teacher) => (
-    <tr
-      key={items.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-shubhPurpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">
-        <Image
-          src={items.photo}
-          alt=""
-          width={40}
-          height={40}
-          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-        />
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{items.name}</h3>
-          <p className="text-xs text-gray-500">{items.email}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{items.teacherId}</td>
-      <td className="hidden md:table-cell">{items.subjects?.join(',')}</td>
-      <td className="hidden md:table-cell">{items.classes?.join(',')}</td>
-      <td className="hidden lg:table-cell">{items.phone}</td>
-      <td className="hidden lg:table-cell">{items.address}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {/* LINK BUTTON */}
-          <FormModal table="teacher" type="update" id={items.id} data={items} />
-          {/* BUTTON */}
-          {role === 'admin' && <FormModal table="teacher" type="delete" id={items.id} />}
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (items: TeacherList) => (
+  <tr
+    key={items.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-shubhPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">
+      <Image
+        src={items.img || '/noAvatar.png'}
+        alt=""
+        width={40}
+        height={40}
+        className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+      />
+      <div className="flex flex-col">
+        <h3 className="font-semibold">{items.name}</h3>
+        <p className="text-xs text-gray-500">{items?.email}</p>
+      </div>
+    </td>
+    <td className="hidden md:table-cell">{items.username}</td>
+    <td className="hidden md:table-cell">{items?.subjects?.join(',')}</td>
+    <td className="hidden md:table-cell">{items?.classes?.join(',')}</td>
+    <td className="hidden lg:table-cell">{items.phone}</td>
+    <td className="hidden lg:table-cell">{items.address}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        {/* LINK BUTTON */}
+        <FormModal table="teacher" type="update" id={items.id} data={items} />
+        {/* BUTTON */}
+        {role === 'admin' && (
+          <FormModal table="teacher" type="delete" id={items.id} />
+        )}
+      </div>
+    </td>
+  </tr>
+);
 
+export default function TeachersList() {
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
